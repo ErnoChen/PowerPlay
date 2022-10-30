ea77e34ea5cbb9f3534050118c9f9d998544311f
Erno <chenerno@gmail.com>
2022-10-29 23:33:10 -0700

    Add initial autonomous; move encoder reset
    Left side autonomous added (blue or red left side start), but distances are not calibrated yet. Resetting the encoder in Gamepad mode has been moved from left/right triggers to a toggle on the start button.

--- Team15091/src/main/java/org/firstinspires/ftc/team15091/OpModeBase.java
+++ Team15091/src/main/java/org/firstinspires/ftc/team15091/OpModeBase.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 public abstract class OpModeBase extends LinearOpMode {
     final protected Robot robot = new Robot();
-    public volatile boolean a_pressed, b_pressed, x_pressed, y_pressed, lb_pressed, rb_pressed, dpad_pressed, back_pressed, guide_pressed;
+    public volatile boolean a_pressed, b_pressed, x_pressed, y_pressed, lb_pressed, rb_pressed, dpad_pressed, back_pressed, guide_pressed, start_pressed;
 
     public final void gamepadUpdate() {
         a_pressed = gamepad1.a || gamepad2.a;
@@ -15,5 +15,6 @@ public abstract class OpModeBase extends LinearOpMode {
         rb_pressed = gamepad1.right_bumper || gamepad2.right_bumper;
         dpad_pressed = gamepad1.dpad_down || gamepad1.dpad_up || gamepad1.dpad_left || gamepad1.dpad_right ||
                 gamepad2.dpad_down || gamepad2.dpad_up || gamepad2.dpad_left || gamepad2.dpad_right;
+        start_pressed = gamepad1.start;
     }
 }
